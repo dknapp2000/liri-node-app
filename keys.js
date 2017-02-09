9a4cb64278ed535570bad3e6e3cd3ed0b1324f7e
Don Knapp <don@dknapp.com>
2017-02-08 22:14:27 -0500

    Added keys

--- keys.js
+++ keys.js
@@ -6,3 +6,12 @@ exports.twitterKeys = {
     access_token_key: '<input here>',
     access_token_secret: '<input here>',
 }
+/* Spotify:
+Client ID
+30b9c5278a3c46d4b6b9e2c9cba804d0
+Client Secret
+b4cce0f5684d4a8498ef39442f624e49
+
+Twitter key:
+4wSA3GgVN8KQO4d9YNu7aFidW
+*/
--- liri.js
+++ liri.js
@@ -3,24 +3,43 @@ var spotify = require("spotify");
 var omdb = require("omdb");
 var keys = require("./keys.js");
 
+switch (action) {
+    case 'twitter-me':
+        break;
+    case 'spotify':
+        break;
+    case 'movie-me':
+        break;
+    default:
+        break;
+}
 // console.log(request);
 
-omdb.search('saw', function(err, movies) {
-    if (err) {
-        return console.error(err);
+// omdb.search('saw', function(err, movies) {
+//     if (err) {
+//         return console.error(err);
+//     }
+
+//     if (movies.length < 1) {
+//         return console.log('No movies were found!');
+//     }
+
+//     movies.forEach(function(movie) {
+//         console.log('%s (%d)', movie.title, movie.year);
+//     });
+
+//     // Saw (2004) 
+//     // Saw II (2005) 
+//     // Saw III (2006) 
+//     // Saw IV (2007) 
+//     // ... 
+// });
+
+spotify.search({ type: 'track', query: 'Adia' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
     }
-
-    if (movies.length < 1) {
-        return console.log('No movies were found!');
-    }
-
-    movies.forEach(function(movie) {
-        console.log('%s (%d)', movie.title, movie.year);
-    });
-
-    // Saw (2004) 
-    // Saw II (2005) 
-    // Saw III (2006) 
-    // Saw IV (2007) 
-    // ... 
+    console.log( JSON.stringify( data ) );
+    
 });
