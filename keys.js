472a5afe9d76b8651287e688c3786ef8918f0b78
Don Knapp <don@dknapp.com>
2017-02-10 22:43:16 -0500

    Twitter is roughed in.

--- keys.js
+++ keys.js
@@ -3,7 +3,7 @@ console.log('Keys.js has been loaded');
 exports.twitterKeys = {
     consumer_key: '4wSA3GgVN8KQO4d9YNu7aFidW',
     consumer_secret: '9rEclGZOHdtRsyjLfl7QhoW2hYGbEqdEDnt7CHKOe3QSXnYrGP',
-    access_token_key: '	3566828487-X4LX3moVxUAlXZepbUIdfda1bM9stIomdIGbKuW',
+    access_token_key: '3566828487-X4LX3moVxUAlXZepbUIdfda1bM9stIomdIGbKuW',
     access_token_secret: 'Wms0Gckf1Rkb6RdERSVTnkcchch4Ve6H1LepxWdms4qMS',
 }
 /* Spotify:
@@ -11,7 +11,7 @@ Client ID
 30b9c5278a3c46d4b6b9e2c9cba804d0
 Client Secret
 b4cce0f5684d4a8498ef39442f624e49
-
+  
 Twitter key:
 4wSA3GgVN8KQO4d9YNu7aFidW
 
--- liri.js
+++ liri.js
@@ -1,11 +1,20 @@
 var request = require("request");
 var spotify = require("spotify");
 var omdb = require("omdb");
+var twitter = require( "twitter" );
 var keys = require("./keys.js");
 var options = [ 'twitter-me', 'spotify', 'movie-me' ];
 var action = process.argv[2];
 
 console.log( keys );
+console.log( keys.twitterKeys.consumer_key );
+
+var client = new twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
 
 switch (action) {
     case 'twitter-me':
@@ -45,6 +54,19 @@ spotify.search({ type: 'track', query: 'Adia' }, function(err, data) {
         console.log('Error occurred: ' + err);
         return;
     }
-    console.log( JSON.stringify( data, null, 2 ) );
+    // console.log( JSON.stringify( data, null, 2 ) );
     
+});
+
+var params = {screen_name: 'dknapp2000'};
+
+client.get('search/tweets', { q: "trump"}, function(error, tweets, response) {
+  if (!error) {
+    //console.log(tweets);
+    for ( var i = 0; i<tweets.statuses.length; i++ ) {
+        console.log( tweets.statuses[i].text)
+    }
+  } else { 
+      console.log( error );
+  }
 });
