1b63c49c2cc6891dec56a7e79e2b1a6b58599b70
Don Knapp <don@dknapp.com>
2017-02-12 20:35:31 -0500

    Spotify working.

--- liri.js
+++ liri.js
@@ -1,13 +1,10 @@
 var request = require("request");
 var spotify = require("spotify");
+var inquirer = require( "inquirer" );
 var omdb = require("omdb");
 var twitter = require( "twitter" );
 var keys = require("./keys.js");
-var options = [ 'twitter-me', 'spotify', 'movie-me' ];
-var action = process.argv[2];
-
-console.log( keys );
-console.log( keys.twitterKeys.consumer_key );
+var options = [ 'my-tweets', 'spotify-this-song', 'movie-this', "do-what-it-says" ];
 
 var client = new twitter({
   consumer_key: keys.twitterKeys.consumer_key,
@@ -16,57 +13,87 @@ var client = new twitter({
   access_token_secret: keys.twitterKeys.access_token_secret
 });
 
+const action = process.argv[2];
+const arg = process.argv.splice(3).join(" ");
+
+console.log( "Action   = " + action );
+console.log( "argument = " + arg );
+
+    
 switch (action) {
-    case 'twitter-me':
+    case 'my-tweets':
+        console.log( "Let's check twitter." );
+        twitterCheck( arg );
         break;
-    case 'spotify':
+    case 'spotify-this-song':
+        console.log( "OK, let's make some music." );
+        spotCheck( arg );
         break;
-    case 'movie-me':
+    case 'movie-this':
+        console.log( "Bring the popcorn!" );
+        omdbCheck( arg );
         break;
+    case 'do-what-it-says':
     default:
         console.log("Pulease choose one of ", options.join(" ") );
         break;
 }
-// console.log(request);
 
-// omdb.search('saw', function(err, movies) {
-//     if (err) {
-//         return console.error(err);
-//     }
 
-//     if (movies.length < 1) {
-//         return console.log('No movies were found!');
-//     }
+function omdbCheck( arg ) {
+    omdb.search( arg || 'saw', function(err, movies) {
+        if (err) {
+            return console.error(err);
+        }
 
-//     movies.forEach(function(movie) {
-//         console.log('%s (%d)', movie.title, movie.year);
-//     });
+        if (movies.length < 1) {
+            return console.log('No movies were found!');
+        }
 
-//     // Saw (2004) 
-//     // Saw II (2005) 
-//     // Saw III (2006) 
-//     // Saw IV (2007) 
-//     // ... 
-// });
+        movies.forEach(function(movie) {
+            console.log('%s (%d)', movie.title, movie.year);
+        });
 
-spotify.search({ type: 'track', query: 'Adia' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-    // console.log( JSON.stringify( data, null, 2 ) );
-    
-});
+        // Saw (2004) 
+        // Saw II (2005) 
+        // Saw III (2006) 
+        // Saw IV (2007) 
+        // ... 
+    });
+}
+
+function spotCheck( arg ) {
+    spotify.search({ type: 'track', query: arg || 'My Zelda' }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        //console.log( JSON.stringify( data, null, 4 ) );
+        //console.log( data.tracks.items[0].album.name );
+        const album = data.tracks.items[0].album.name;
+        const track = data.tracks.items[0].name;
+        const preview = data.tracks.items[0].preview_url;
+        let artist = "";
+        for ( let i = 0; i<data.tracks.items[0].album.artists.length; i++ ) {
+            artist ? artist = artist + ", " + data.tracks.items[0].album.artists[i].name : artist = data.tracks.items[0].album.artists[i].name;
+        }
+        console.log( "artist = ", artist );
+        console.log( "track  = ", album );
+        console.log( "Track  = ", track );
+        console.log( "Preview= ", preview );
+    });
+}
 
 var params = {screen_name: 'dknapp2000'};
 
-client.get('search/tweets', { q: "trump"}, function(error, tweets, response) {
-  if (!error) {
-    //console.log(tweets);
-    for ( var i = 0; i<tweets.statuses.length; i++ ) {
-        console.log( tweets.statuses[i].text)
-    }
-  } else { 
-      console.log( error );
-  }
-});
+function twitterCheck( arg ) { 
+    client.get('search/tweets', { q: "node.js"}, function(error, tweets, response) {
+        if (!error) {
+            for ( var i = 0; i<tweets.statuses.length; i++ ) {
+                console.log( tweets.statuses[i].text)
+            }
+        } else { 
+            console.log( error );
+        }
+    });
+}
